9bc6e101fdec6cc47eea0570a54b79fb3862f492
Chris-N-Wu <77757549+Chris-N-Wu@users.noreply.github.com>
2021-04-21 21:22:27 -0400

    spring cleaning
    made these functions callable
    did some cleaning

--- albertsteal.py
+++ albertsteal.py
@@ -43,81 +43,102 @@ finishedTab = WebDriverWait(driver,10).until(
     EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div[3]/div/div[2]/div/div/div[1]/button[3]"))
 ).click()
 
-assignments = WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, "sg-table__tr--link"))
-)
+def assignmentLoader():
+    assignments = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "sg-table__tr--link"))
+    )
 
-#make folders: data/classname/assignments
-
-for i in range(len(assignments)):                   #works
-    xpath = '//*[@id="app"]/div/div[1]/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div[2]/table/tbody/tr[{}]'.format(i+1)
-    time.sleep(1.25)
-    assignment = driver.find_element_by_xpath(xpath) 
-    assignment.click()
-    
-    assignmentTitle = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "u-mar-b_1"))
-    ).text
-    assignmentTitle = assignmentTitle.replace(":","").replace('/',' ').replace("?","").replace("*","").replace("/","").replace("<","").replace(">","").replace('\n' ,"").replace('\\', '').strip()
-    
-    print(assignmentTitle)
-
-    question = input("Do you want to copy this?: (Y/N): ")
-    if question == "Y" or question == "y":
-        try:
-            os.mkdir(f'images/{assignmentTitle}')
-        except Exception as exception:
-            print(exception)
-               
-        start = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,"//*[text()='View Questions']"))
-        ).click()
+    for i in range(len(assignments)):                   #works
+        xpath = '//*[@id="app"]/div/div[1]/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div[2]/table/tbody/tr[{}]'.format(i+1)
+        time.sleep(1.25)
+        assignment = driver.find_element_by_xpath(xpath) 
+        assignment.click()
         
-        nextButton = True
-        while nextButton:
-            try: 
-                clickNext = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, "//*[@id='app']/div/div[1]/div/div[3]/div[2]/div/div[2]/div/form/div[1]/div/button[2]"))
-                )
-                time.sleep(1)
-                title = driver.find_element_by_xpath("//*[@id='app']/div/div[1]/div/div[3]/div[2]/div/div[2]/div/form/div[1]/div/div/h1/div/div")
-                title = title.text
-                title = title.replace(":","").replace('/',' ').replace("?","").replace("*","").replace("/","").replace("<","").replace(">","").replace('\n' ,"").replace('\\', '').strip()
+        assignmentTitle = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "u-mar-b_1"))
+        ).text
+        assignmentTitle = assignmentTitle.replace(":","").replace('/',' ').replace("?","").replace("*","").replace("/","").replace("<","").replace(">","").replace('\n' ,"").replace('\\', '').strip()
+        
+        print(assignmentTitle)
 
-                if os.path.isfile(f'images/{assignmentTitle}/{title}.png'):
-                    title = title + str(i)
-                    i += 1
-
-                driver.save_screenshot(f'images/{assignmentTitle}/{title}.png')
-
-                start_element = driver.find_element_by_class_name('question-wrapper__heading')
-                start_location = start_element.location
-                start_x = start_location['x']
-                start_y = start_location['y']
+        question = input("Do you want to copy this?: (Y/N): ")
+        if question == "Y" or question == "y":
+            try:
+                os.mkdir(f'images/{assignmentTitle}')
+            except Exception as exception:
+                print(exception)
                 
-                end_element = driver.find_element_by_class_name('m-banner')
-                end_size = end_element.size
-                end_location = end_element.location
-                end_x = end_location['x'] + end_size['width']
-                end_y = end_location['y']
+            start = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,"//*[text()='View Questions']"))
+            ).click()
+            
+            nextButton = True
+            while nextButton:
+                try: 
+                    clickNext = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.XPATH, "//*[@id='app']/div/div[1]/div/div[3]/div[2]/div/div[2]/div/form/div[1]/div/button[2]"))
+                    )
+                    time.sleep(1)
+                    title = driver.find_element_by_xpath("//*[@id='app']/div/div[1]/div/div[3]/div[2]/div/div[2]/div/form/div[1]/div/div/h1/div/div")
+                    title = title.text
+                    title = title.replace(":","").replace('/',' ').replace("?","").replace("*","").replace("/","").replace("<","").replace(">","").replace('\n' ,"").replace('\\', '').strip()
 
-                #check if question titles are the same
-                img = Image.open(f'images/{assignmentTitle}/{title}.png')
-                img = img.crop((start_x, start_y, end_x, end_y))
-                img.save(f'images/{assignmentTitle}/{title}.png')
+                    if os.path.isfile(f'images/{assignmentTitle}/{title}.png'):
+                        title = title + str(i)
+                        i += 1
+
+                    driver.save_screenshot(f'images/{assignmentTitle}/{title}.png')
+
+                    start_element = driver.find_element_by_class_name('question-wrapper__heading')
+                    start_location = start_element.location
+                    start_x = start_location['x']
+                    start_y = start_location['y']
+                    
+                    end_element = driver.find_element_by_class_name('m-banner')
+                    end_size = end_element.size
+                    end_location = end_element.location
+                    end_x = end_location['x'] + end_size['width']
+                    end_y = end_location['y']
+
+                    #check if question titles are the same
+                    img = Image.open(f'images/{assignmentTitle}/{title}.png')
+                    img = img.crop((start_x, start_y, end_x, end_y))
+                    img.save(f'images/{assignmentTitle}/{title}.png')
 
 
-                if clickNext.get_property('disabled'):
-                    nextButton = False
-                else:
-                    clickNext.click()
+                    if clickNext.get_property('disabled'):
+                        nextButton = False
+                    else:
+                        clickNext.click()
 
-            finally:
-                print(nextButton)
-    elif question == "N" or question == "n":
+                finally:
+                    continue
+            print("Finished Copying: " + assignmentTitle + '\n')
+        elif question == "N" or question == "n":
+            pass
+        else:
+            break
+        driver.back()
+
+def pageNavigation():
+    try:
+        page = input("Assignment page to begin: (1/2/3...): ")
+        nextAssignmentList = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, f'//*[@id="app"]/div/div[1]/div/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div[{page}]'))
+        ).click()
+
+        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+        assignmentLoader()
+    except:
+        print("Something didn't work")
         pass
-    else:
-        break
-    driver.back()
+
+def masterController():
+    continueProgram = True
+    while continueProgram == True or continueProgram == "true":
+        pageNavigation()
+        continueProgram = input("Would you like to continue this program?: (True/False): ") #any response other than "True" will end program
+
+masterController()
 
 driver.quit()
